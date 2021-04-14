49981dc73e702d557bf0364eaafd8f1f051afd22
ikam <els0212@nate.com>
2021-04-14 22:08:11 +0900

    study computed & watch

--- index/index.js
+++ index/index.js
@@ -71,3 +71,53 @@ var app7 = new Vue({
     ]
   }
 })
+
+var app8 = new Vue({
+  el:"#app-8",
+  data:{
+    rawHtml:"<span style=\"color:red\">This should be red.</span>",
+  }
+})
+
+
+var app9 = new Vue({
+  el:'#app-9',
+  data:{
+    url:"index.js"
+  }
+})
+
+
+var app10 = new Vue({
+  el:"#app-10",
+  data:{
+    question: '',
+    answer: '질문을 하기 전까지는 대답할 수 없습니다.'
+  },
+  watch:{
+    question: function (newQuestion){
+      this.answer = '입력을 기다리는 중...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function(){
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function(){
+      if (this.question.indexOf('?') === -1){
+        this.answer = "질문에는 일반적으로 물음표가 포함됩니다. ;-)"
+        return
+      }
+      this.answer = '생각중...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+      .then(function (response){
+        vm.answer = _.capitalize(response.data.answer)
+      })
+      .catch(function (error){
+        vm.answer = '에러! API 요청에 오류가 있습니다.' + error
+      })
+    }
+  }
+})
